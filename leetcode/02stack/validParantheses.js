0aedcff162d5ec5e39636f885c9a4fd5b247cea0
Gayathri Gireesh Sujatha <gayathri.sujatha@arup.com>
2024-07-17 14:37:20 +0530

    folder

---
+++ leetcode/02stack/validParantheses.js
@@ -0,0 +1,50 @@
+// 20. Valid Parentheses
+// Solved
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type.
+
+// Example 1:
+
+// Input: s = "()"
+// Output: true
+// Example 2:
+
+// Input: s = "()[]{}"
+// Output: true
+// Example 3:
+
+// Input: s = "(]"
+// Output: false
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+const isValid = (s) => {
+  const stack = [];
+  const matchingBrackets = {
+    ")": "(",
+    "}": "{",
+    "]": "[",
+  };
+  for (let char of s) {
+    if (char === "(" || char === "{" || char === "[") stack.push(char);
+    else if (char === ")" || char === "}" || char === "]")
+      if (stack.length === 0 || stack.pop() !== matchingBrackets[char]) return false;
+  }
+  return stack.length === 0
+};
+
+console.log('testcase'+ isValid('{{]}}'))
+console.log('testcase'+ isValid('{}}}'))
+console.log('testcase'+ isValid('{[]}{}'))
+console.log('testcase'+ isValid('{[][{}}{]}}'))
+
+
